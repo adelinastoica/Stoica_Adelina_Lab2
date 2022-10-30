20d17b4d7a455d8492e56f739e721827c8b7ac05
Adelina <Adelina@DESKTOP-5JMB76T>
2022-10-30 19:41:54 +0200

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Stoica_Adelina_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
